2a5194349cbb8479661d4aabffd84943c9debca1
Matt Olenik <olenikm@gmail.com>
2018-07-13 20:12:28 -0700

    Major fixes and cleanup, seems to handle all cases

--- args.sh
+++ args.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 VERSION=0.1.0
 fail() { printf '%s\n' "$*"; exit 1; }
-NONE=@__BASH_NONE__@
+NONE=$'\31'NONE
 
 print_usage() {
   echo usage goes here
@@ -12,10 +12,10 @@ print_usage() {
 ##
 # Argument parsing
 ##
-handle_option() {
-  #local dash=$1
+handle_argument() {
+  local dash=$1
   local name=$2
-  local value=${3:-$NONE}
+  local value=${3:-}
   case $name in
     o|oops)
       echo "oopies $value!"
@@ -30,7 +30,7 @@ handle_option() {
       ;;
     *)
       print_usage 1>&2
-      fail "Unknown argument $1"
+      fail "Unknown argument $dash$name"
       ;;
   esac
 }
@@ -41,34 +41,50 @@ handle_positionals() {
 
 parse_args() {
   local quot_word="\"(.*)\"|'(.*)'|(.*)"
-  local single_dash_argument="^-([^[:blank:]]+)"
-  local double_dash_argument="^--([^[:blank:]]+)(=|[[:blank:]]+)($quot_word)$"
-  local name value
-  local -a args
-  local -a positionals
-  args=("$@")
-  positionals=()
+  local single_dash_argument="^-([^[:blank:]]+)$"
+  local double_dash_argument="^--([^[:blank:]=]+)($|=)($quot_word)?"
+  local arg name next_arg value
+  local args=("$@")
+  local positionals=()
   i=0
   while (( i < $# )); do
     arg="${args[$i]}"
+    next_arg="${args[$i+1]:-}"
     if [[ $arg =~ $double_dash_argument ]]; then
+      # This if block handles the case of double-dash arguments with an equals
+      # sign, e.g. --output=filename. It handles quotes around the argument.
       name="${BASH_REMATCH[1]}"
-      # The value will be one of the three capture groups that handle double
-      # quotes, single quotes, or no quotes. Only one group can match at a
-      # time, so concatenating is the easiest way to get the value for any
-      # kind of quoting.
-      value="${BASH_REMATCH[4]${BASH_REMATCH[5]}}${BASH_REMATCH[6]}"
-      handle_option -- "$name" "$value"
+      if [[ -n ${BASH_REMATCH[2]} ]]; then
+        # The value will be one of the three capture groups that handle double
+        # quotes, single quotes, or no quotes. Only one group can match at a
+        # time, so concatenating is the easiest way to get the value for any
+        # kind of quoting.
+        value="${BASH_REMATCH[4]}${BASH_REMATCH[5]}${BASH_REMATCH[6]}"
+        handle_argument -- "$name" "$value"
+      else
+        # If the next argument isn't another argument (doesn't begin with
+        # a dash), treat it as the value for the current argument.
+        if (( i + 1 == $# )) || [[ $next_arg == -* ]]; then
+          handle_argument -- "$name"
+        else
+          handle_argument -- "$name" "$next_arg"
+        fi
+      fi
     elif [[ "$arg" =~ $single_dash_argument ]]; then
       # This elif block handles single-letter args and groups of args,
       # e.g. '-o "filename"' or '-vfd' which becomes -v -f -d
       name="${BASH_REMATCH[1]}"
+      # Groupings of options are always flags without values
       if (( ${#name} > 1 )); then
-        for (( i=0; i<${#name}; i++ )); do
-          handle_option - "${name:$i:1}"
+        for (( k=0; k<${#name}; k++ )); do
+          handle_argument - "${name:$k:1}"
         done
       else
-        handle_option - "$name"
+        if (( i + 1 == $# )) || [[ $next_arg == -* ]]; then
+          handle_argument - "$name"
+        else
+          handle_argument - "$name" "$next_arg"
+        fi
       fi
     else
       positionals+=("$arg")
