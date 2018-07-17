d001a3bc81be89224039f6d1df0cc29229ceddcb
Matt Olenik <olenikm@gmail.com>
2018-07-16 23:27:31 -0700

    bats

--- args.sh
+++ args.sh
@@ -1,105 +1,64 @@
 #!/usr/bin/env bash
 [[ -n ${TRACE:-} ]] && set -x
-set -euo pipefail
-VERSION=0.1.0
-fail() { printf '%s\n' "$*"; exit 1; }
-
-print_usage() {
-  cat <<EOF
-Usage information
-
-EOF
-}
+[[ -z ${IS_TEST:-} ]] && set -euo pipefail
 
 ##
-# Argument parsing
-##
-handle_argument() {
-  local dash=$1
-  local name=$2
-  local value=${3:-}
-  case $name in
-    o|oops)
-      echo "oopies $value!"
-      ;;
-    a)
-      echo "set a" ;;
-    b)
-      echo "set b" ;;
-    C)
-      echo "set C" ;;
-    h|help)
-      print_usage
-      exit 0
-      ;;
-    v|version)
-      echo "$VERSION"
-      exit 0
-      ;;
-    *)
-      print_usage 1>&2
-      fail "Unknown argument $dash$name"
-      ;;
-  esac
-}
-
-handle_positionals() {
-  echo "Positionals: $*"
-}
-
+# Parses command-line arguments
+# Parameters:
+# $1  name of the function to handle options
+# $2  name of the function to handle positionals
+# ..  remaining are the arguments to parse
 parse_args() {
+  local handle_option=$1 handle_positionals=$2
+  shift 2
   local quot_word="\"(.*)\"|'(.*)'|(.*)"
-  # Matches single char args e.g. -a or groups of args e.g. -abx
-  local single_dash_argument="^-([^[:blank:]]+)$"
-  # Matches a double dash argument that may or may not have a value
-  local double_dash_argument="^--([^[:blank:]=]+)($|=)($quot_word)?"
-  local arg name next_arg value
-  local args=("$@")
-  local positionals=()
-  i=0
+  # Matches single char opts e.g. -a or groups of opts e.g. -abx
+  local single_dash_option="^-([^[:blank:]]+)$"
+  # Matches a double dash option that may or may not have a value
+  local double_dash_option="^--([^[:blank:]=]+)($|=)($quot_word)?"
+  local name next_arg opt value
+  local args=("$@") positionals=()
+  local i=0
   while (( i < $# )); do
-    arg="${args[$i]}"
+    opt="${args[$i]}"
     next_arg="${args[$i+1]:-}"
-    if [[ $arg =~ $double_dash_argument ]]; then
-      # This if block handles the case of double-dash arguments with an equals
-      # sign, e.g. --output=filename. It handles quotes around the argument.
+    if [[ $opt =~ $double_dash_option ]]; then
+      # This if block handles the case of double-dash options with an equals
+      # sign, e.g. --output=filename. It handles quotes around the option.
       name="${BASH_REMATCH[1]}"
       if [[ -n ${BASH_REMATCH[2]} ]]; then
         # The value of the quoted/unquoted string ends up in one of three groups
         value="${BASH_REMATCH[4]}${BASH_REMATCH[5]}${BASH_REMATCH[6]}"
-        handle_argument -- "$name" "$value"
+        $handle_option -- "$name" "$value"
       else
-        # If the next argument isn't another argument (doesn't begin with
-        # a dash), treat it as the value for the current argument.
+        # If the next option isn't another option (doesn't begin with
+        # a dash), treat it as the value for the current option.
         if (( i + 1 == $# )) || [[ $next_arg == -* ]]; then
-          handle_argument -- "$name"
+          $handle_option -- "$name"
         else
-          handle_argument -- "$name" "$next_arg"
+          $handle_option -- "$name" "${args[@]:1}"
+          i=$((i + $?))
         fi
       fi
-    elif [[ "$arg" =~ $single_dash_argument ]]; then
-      # This elif block handles single-letter args and groups of args,
+    elif [[ "$opt" =~ $single_dash_option ]]; then
+      # This elif block handles single-letter opts and groups of opts,
       # e.g. '-o "filename"' or '-vfd' which becomes -v -f -d
       name="${BASH_REMATCH[1]}"
-      # Groupings of options are always flags without values
-      if (( ${#name} > 1 )); then
-        for (( k=0; k<${#name}; k++ )); do
-          handle_argument - "${name:$k:1}"
-        done
+      # Groups of options can't have values, except for the last one.
+      for (( k=0; k < ${#name} - 1; k++ )); do
+        $handle_option - "${name:$k:1}"
+      done
+      # Don't pass a value if on the last opt or if the next opt is an option
+      if (( i + 1 == $# )) || [[ $next_arg == -* ]]; then
+        $handle_option - "${name: -1}"
       else
-        # Don't pass a value if on the last arg or if the next arg is an option
-        if (( i + 1 == $# )) || [[ $next_arg == -* ]]; then
-          handle_argument - "$name"
-        else
-          handle_argument - "$name" "$next_arg"
-        fi
+        $handle_option - "${name: -1}" "${args[@]:1}"
+        i=$((i + $?))
       fi
     else
-      positionals+=("$arg")
+      positionals+=("$opt")
     fi
     i=$((i + 1))
   done
-  handle_positionals "${positionals[@]}"
+  $handle_positionals "${positionals[@]}"
 }
-
-parse_args "$@"
--- tests.bats
+++ tests.bats
@@ -1,5 +1,21 @@
 #!/usr/bin/env bats
+IS_TEST=true
+source args.sh
+
+arguments=()
+positionals=()
 
 @test "test" {
-  [[ 1 == 1 ]]
+  args=(-a -b --test=abc)
+  parse_args handle_argument handle_positionals "${args[@]}"
+  echo ${arguments[@]} 1>&2
+}
+
+handle_argument() {
+  arguments+=(${1}${2}${3:-})
+  echo $* &> test.out
+}
+
+handle_positionals() {
+  positionals="$@"
 }
